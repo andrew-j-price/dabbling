f9d3b32083b3bf812e9801ef2a43cb350f0cdb4b
Andrew Price <andrpric@cisco.com>
2016-10-16 21:50:11 +0000

    add validation

---
+++ roles/deploy/files/validate.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import requests
+import sys
+
+
+def default():
+    url = 'http://127.0.0.1:80'
+    r = requests.get(url, timeout=3.0)
+    if r.status_code == 200 and len(r.text) == 13:
+        print('SUCCESS! Respone returned: ' + r.text)
+        sys.exit(0)
+    else:
+        print('FAILURE')
+        sys.exit(2)
+
+
+if __name__ == "__main__":
+    try:
+        default()
+    except Exception as e:
+        print('EXCEPTION_RAISED: ' + str(e))
+        sys.exit(3)
+    except KeyboardInterrupt:
+        sys.exit(1)
